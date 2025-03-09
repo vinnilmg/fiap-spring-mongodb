f9f1c1c144a2c590fb359b101e2f0422c1dd1fad
= <vini.delimagomes@gmail.com>
2025-03-09 20:53:44 -0300

    add repository and service impl

---
+++ src/main/java/com/fiap/spring_mongo_db/service/ArtigoServiceImpl.java
@@ -0,0 +1,32 @@
+package com.fiap.spring_mongo_db.service;
+
+import com.fiap.spring_mongo_db.model.Artigo;
+import com.fiap.spring_mongo_db.repository.ArtigoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ArtigoServiceImpl implements ArtigoService {
+    private final ArtigoRepository repository;
+
+    public ArtigoServiceImpl(ArtigoRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public List<Artigo> obterTodos() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Artigo obterPorCodigo(final String codigo) {
+        return repository.findById(codigo)
+                .orElseThrow(() -> new IllegalArgumentException("Artigo não existe"));
+    }
+
+    @Override
+    public Artigo criar(final Artigo artigo) {
+        return repository.save(artigo);
+    }
+}
