de71791689d4207f94b4f24535f1203852a59807
= <vini.delimagomes@gmail.com>
2025-03-09 20:19:29 -0300

    init proj

---
+++ src/main/java/com/fiap/spring_mongo_db/ArtigoBlogApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.spring_mongo_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtigoBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtigoBlogApplication.class, args);
+	}
+
+}
